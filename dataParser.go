b67aff3fb54e8897a114c330a44475569d56dc1a
arosa822 <h.rosa822@gmail.com>
2019-10-22 19:44:11 -0400

    convert map object to structure

--- dataParser.go
+++ dataParser.go
@@ -10,6 +10,11 @@ import (
 	"strings"
 )
 
+type hitList struct {
+	User      string `json: "user"`
+	TimeStamp []int  `json: "timeStamp"`
+}
+
 // readLines reads a whole file into memory and returns a slice of its lines.
 func readLines(path string) ([]string, error) {
 	file, err := os.Open(path)
@@ -55,6 +60,21 @@ func processByTime(list *[]string) map[string][]int {
 	return mapOfUsers
 }
 
+// convertMapToStruct takes in a map object as an argument and returns a
+// mapped hitList structure defined above
+// returned object needs to have key value as a string for compatibility with
+// encoding into JSON
+func convertMapToStruct(unstructData *map[string][]int) map[string]hitList {
+	data := make(map[string]hitList)
+	count := 1
+	for key, value := range *unstructData {
+		// throw key value pairs in structure and convert the count to string
+		data[strconv.Itoa(count)] = hitList{User: key, TimeStamp: value}
+		count++
+	}
+	return data
+}
+
 func writeToFile(json *[]uint8) {
 	err := ioutil.WriteFile("output.json", *json, 0644)
 	if err != nil {
@@ -71,7 +91,9 @@ func main() {
 
 	filteredList := processByTime(&listOfUsers)
 
-	jsonString, err := json.Marshal(filteredList)
+	siteVisits := convertMapToStruct(&filteredList)
+
+	jsonString, err := json.Marshal(siteVisits)
 	if err != nil {
 		panic(err)
 	}
